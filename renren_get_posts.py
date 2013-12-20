7808334fa943419b5a72564cc9d35c3c4f678b5b
laike9m <laike9m@gmail.com>
2013-12-20 21:40:59 +0800

    create html and markdown folder

--- renren_get_posts.py
+++ renren_get_posts.py
@@ -1,4 +1,8 @@
 import requests
+import os
+
+HTML_DIR = 'original html'
+DUMP_DIR = 'markdown'
 
 class LoginRenRen():  
     
@@ -21,29 +25,37 @@ class LoginRenRen():
         print('login.....')  
         
         print(r.url)
-        with open('test.html', mode='w', encoding='utf-8') as f:
+        with open(os.path.join(HTML_DIR,'test.html'), mode='w', encoding='utf-8') as f:
             f.write(r.text)
 
 
 class Get_Blogpost(LoginRenRen):
     
-    def __init__(self):
+    def __init__(self, name, password, domain):
+        self.name=name  
+        self.password=password  
+        self.domain=domain  
         self.test_post_url = 'http://blog.renren.com/blog/282456584/917107448'  # 日志《漫画家从良》
 
     def get_test_post(self):
         r = self.s.get(self.test_post_url)
-        with open('test_post.html', mode='w', encoding='utf-8') as f:
+        with open(os.path.join(DUMP_DIR,'test_post.html'), mode='w', encoding='utf-8') as f:
             f.write(r.text)  
               
               
-if __name__=='__main__':  
+if __name__=='__main__':
+    0 if os.path.exists(HTML_DIR) else os.mkdir(HTML_DIR)
+    0 if os.path.exists(DUMP_DIR) else os.mkdir(DUMP_DIR)
+     
+      
     username = input('请输入用户名: ')
     password = input('请输入密码: ')
     domain = 'renren.com'  
-    ren = LoginRenRen(username, password, domain)  
-    ren.login()
+    #ren = LoginRenRen(username, password, domain)  
+    #ren.login()
     
-    ren_get_blogpost = Get_Blogpost()
+    ren_get_blogpost = Get_Blogpost(username, password, domain)
+    ren_get_blogpost.login()
     ren_get_blogpost.get_test_post()
     
     
